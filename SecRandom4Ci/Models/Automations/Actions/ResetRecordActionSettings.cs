48102f56d040c02d13a23590a81ff7009c651c82
lrs2187 <liurongshuo2022@outlook.com>
2026-02-05 23:04:21 +0800

    feat: 添加「重置 SecRandom 记录」行动

---
+++ SecRandom4Ci/Models/Automations/Actions/ResetRecordActionSettings.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SecRandom4Ci.Enums;
+
+namespace SecRandom4Ci.Models.Automations.Actions;
+
+public partial class ResetRecordActionSettings : ObservableRecipient
+{
+    [ObservableProperty] private SecRandomRecordMode _mode = SecRandomRecordMode.RollCall;
+}
---
+++ SecRandom4Ci/Services/Automations/Actions/ResetRecordAction.cs
@@ -0,0 +1,25 @@
+﻿using ClassIsland.Core.Abstractions.Automation;
+using ClassIsland.Core.Attributes;
+using SecRandom4Ci.Enums;
+using SecRandom4Ci.Models.Automations.Actions;
+using SecRandom4Ci.Shared;
+
+namespace SecRandom4Ci.Services.Automations.Actions;
+
+[ActionInfo("secrandom4ci.actions.resetRecord", "重置 SecRandom 记录", "\uED09")]
+public class ResetRecordAction : ActionBase<ResetRecordActionSettings>
+{
+    protected override async Task OnInvoke()
+    {
+        var url = Settings.Mode switch
+        {
+            SecRandomRecordMode.RollCall => "secrandom://roll_call/reset",
+            SecRandomRecordMode.Lottery => "secrandom://lottery/reset",
+            _ => string.Empty
+        };
+        
+        if (url == string.Empty) return;
+
+        _ = SecRandomIpcSendUrl.SendUrlAsync(url);
+    }
+}
