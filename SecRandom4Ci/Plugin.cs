e7d3a25a524c4eacc822820a48e1e26c3a8427cd
lrs2187 <liurongshuo2022@outlook.com>
2026-02-05 23:38:20 +0800

    feat: sai 适配

--- SecRandom4Ci/Plugin.cs
+++ SecRandom4Ci/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using ClassIsland.Core;
 using ClassIsland.Core.Abstractions;
+using ClassIsland.Core.Abstractions.Services;
 using ClassIsland.Core.Attributes;
 using ClassIsland.Core.Extensions.Registry;
 using ClassIsland.Shared;
@@ -13,6 +14,10 @@ using SecRandom4Ci.Models.Automations.Triggers;
 using SecRandom4Ci.Services;
 using SecRandom4Ci.Services.Automations;
 using SecRandom4Ci.Services.Automations.Actions;
+using SuperAutoIsland.Interface;
+using SuperAutoIsland.Interface.MetaData;
+using SuperAutoIsland.Interface.MetaData.ArgsType;
+using SuperAutoIsland.Interface.Services;
 
 namespace SecRandom4Ci;
 
@@ -40,6 +45,116 @@ public class Plugin : PluginBase
         {
             IAppHost.GetService<SecRandomService>();
             IAppHost.GetService<RuleHandlerService>().Register();
+            
+            
+            if (IsPluginInstalled("lrs2187.sai"))
+            {
+                RegisterSaiBlocks();
+            }
         };
+    }
+    
+    private static bool IsPluginInstalled(string pkgName, Version? version = null)
+    {
+        return IPluginService.LoadedPlugins.Any(info => info.Manifest.Id == pkgName && info.IsEnabled && new Version(info.Manifest.Version) >= version);
+    }
+
+    private static void RegisterSaiBlocks()
+    {
+        var saiServerService = IAppHost.GetService<ISaiServer>();
+        saiServerService.RegisterBlocks("SecRandom-Ci", new RegisterData
+        {
+            Actions =
+            [
+                new BlockMetadata
+                {
+                    Id = "secrandom4ci.actions.resetRecord",
+                    Name = "重置 SecRandom 记录",
+                    Icon = ("删除人员", "\uED09"),
+                    Args = new Dictionary<string, MetaArgsBase>
+                    {
+                        ["Mode"] = new DropDownMetaArgs
+                        {
+                            Name = "",
+                            Type = MetaType.dropdown,
+                            Options = [
+                                ("点名/闪抽", "0"),
+                                ("抽奖", "1")
+                            ]
+                        }
+                    },
+                    DropdownUseNumbers = true,
+                    InlineField = false,
+                    InlineBlock = false,
+                    IsRule = false
+                }
+            ],
+            Rules =
+            [
+                new BlockMetadata
+                {
+                    Id = "secrandom4ci.rules.lastCalledPerson",
+                    Name = "SecRandom 上次抽到",
+                    Icon = ("人员可用", "\uECF9"),
+                    Args = new Dictionary<string, MetaArgsBase>
+                    {
+                        ["_string1"] = new CommonMetaArgs
+                        {
+                            Name = "",
+                            Type = MetaType.dummy
+                        },
+                        ["OnRollCall"] = new CommonMetaArgs
+                        {
+                            Name = "包含点名",
+                            Type = MetaType.boolean
+                        },
+                        ["OnQuickDraw"] = new CommonMetaArgs
+                        {
+                            Name = "包含闪抽",
+                            Type = MetaType.boolean
+                        },
+                        ["OnLottery"] = new CommonMetaArgs
+                        {
+                            Name = "包含抽奖",
+                            Type = MetaType.boolean
+                        },
+                        ["FilterByPersonName"] = new CommonMetaArgs
+                        {
+                            Name = "按姓名筛选",
+                            Type = MetaType.boolean
+                        },
+                        ["PersonName"] = new CommonMetaArgs
+                        {
+                            Name = "姓名",
+                            Type = MetaType.text
+                        },
+                        ["FilterByGroupName"] = new CommonMetaArgs
+                        {
+                            Name = "按组名筛选",
+                            Type = MetaType.boolean
+                        },
+                        ["GroupName"] = new CommonMetaArgs
+                        {
+                            Name = "组名",
+                            Type = MetaType.text
+                        },
+                        ["FilterByLotteryName"] = new CommonMetaArgs
+                        {
+                            Name = "按奖项筛选",
+                            Type = MetaType.boolean
+                        },
+                        ["LotteryName"] = new CommonMetaArgs
+                        {
+                            Name = "奖项",
+                            Type = MetaType.text
+                        }
+                    },
+                    DropdownUseNumbers = false,
+                    InlineField = false,
+                    InlineBlock = false,
+                    IsRule = true
+                }
+            ]
+        });
     }
 }
